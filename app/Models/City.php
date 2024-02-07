b2dc0f2fab073220b22c1783a734ce08d2da14c9
dedyunej <dedy3@unej.ac.id>
2024-02-08 05:13:43 +0700

    first commit

---
+++ app/Models/City.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class City extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'state_id','id'];
+
+    public function state():BelongsTo
+    {
+        return $this->belongsTo(State::class);
+
+    }
+}
